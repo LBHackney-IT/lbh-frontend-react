c8e15aa8845ac0824bffe666060b09de7f28b2ac
F <f@dxw.com>
2019-11-04 21:38:27 +0000

    Set up individual component builds
    We export ES module syntax for the individual components, as most React
    build systems will already support that syntax. We still export a
    CommonJS version with all components bundled in for those users who
    are unable to use ES modules.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 /* eslint-env node */
 const babel = require("rollup-plugin-babel");
 const del = require("rollup-plugin-delete");
+const { default: multiInput } = require("rollup-plugin-multi-input");
 const resolve = require("rollup-plugin-node-resolve");
 const postcss = require("rollup-plugin-postcss");
 const progress = require("rollup-plugin-progress");
@@ -9,46 +10,58 @@ const typescript = require("rollup-plugin-typescript2");
 const pkg = require("./package.json");
 const tsconfig = require("./tsconfig.json");
 
-module.exports = {
-  input: "src/index.ts",
-  output: [
-    {
+const external = [
+  ...Object.keys(pkg.dependencies || {}),
+  ...Object.keys(pkg.peerDependencies || {})
+];
+
+const plugins = [
+  progress({
+    clearLine: false
+  }),
+  resolve(),
+  postcss(),
+  typescript({
+    typescript: require("typescript"),
+    tsconfigOverride: {
+      exclude: [
+        ...tsconfig.exclude,
+        "**/__tests__/**/*",
+        "**/*.spec.*",
+        "**/*.test.*"
+      ]
+    }
+  }),
+  babel({
+    extensions: [".ts", ".tsx"],
+    exclude: "**/node_modules/**/*"
+  })
+];
+
+module.exports = [
+  {
+    input: ["src/**/*.ts?(x)", "!**/*.(spec|test).*", "!**/__tests__/**/*"],
+    output: {
+      dir: "dist",
+      format: "es"
+    },
+    external,
+    plugins: [
+      multiInput(),
+      del({
+        targets: ["dist/**/*"],
+        verbose: true
+      }),
+      ...plugins
+    ]
+  },
+  {
+    input: "src/index.ts",
+    output: {
       file: pkg.main,
       format: "cjs"
     },
-    {
-      file: pkg.module,
-      format: "es"
-    }
-  ],
-  external: [
-    ...Object.keys(pkg.dependencies || {}),
-    ...Object.keys(pkg.peerDependencies || {})
-  ],
-  plugins: [
-    del({
-      targets: ["dist/**/*"],
-      verbose: true
-    }),
-    progress({
-      clearLine: false
-    }),
-    resolve(),
-    postcss(),
-    typescript({
-      typescript: require("typescript"),
-      tsconfigOverride: {
-        exclude: [
-          ...tsconfig.exclude,
-          "**/__tests__/**/*",
-          "**/*.spec.*",
-          "**/*.test.*"
-        ]
-      }
-    }),
-    babel({
-      extensions: [".ts", ".tsx"],
-      exclude: "**/node_modules/**/*"
-    })
-  ]
-};
+    external,
+    plugins
+  }
+];

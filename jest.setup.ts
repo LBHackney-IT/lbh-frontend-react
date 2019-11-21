4d4c7a1bcae7561100ccfe3c495a97935cd87755
Chris <cjwaszczuk0@Live.com>
2019-11-21 14:16:34 +0000

    Setup Enzyme with Jest
    We need to use enzyme with Jest so that we can make use of
    simulate and click testing.

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,4 +1,9 @@
+import Adapter from "enzyme-adapter-react-16";
+import { configure } from "enzyme";
+
 import { ComponentRegister } from "./src/ComponentRegister";
+
+configure({ adapter: new Adapter() });
 
 afterEach(() => {
   ComponentRegister.reset();

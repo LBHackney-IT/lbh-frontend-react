1cb7e78036de52e46d158692927d674854dfbeb5
Christopher Parsons <mail@christopherparsons.me>
2020-07-10 12:23:11 +0100

    Stop storybook from being date dependant
    Co-authored-by: LBHABEBIC <Ana.Bebic@hackney.gov.uk>
    Co-authored-by: tony-griffin <tony@madetech.com>

--- stories/Worktray.stories.tsx
+++ stories/Worktray.stories.tsx
@@ -1,6 +1,11 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { WorkTray, Status } from "../src/components/Worktray/Worktray";
+import moment from "moment";
+
+const currentDate = moment(new Date());
+const circleDate = moment(currentDate).add(3, "days").format("DD/MM/YYYY");
+const priorDate = moment(currentDate).subtract(1, "days").format("DD/MM/YYYY");
 
 const designData = [
   {
@@ -23,7 +28,7 @@ const designData = [
       },
       {
         key: "dueCompletion",
-        value: "12/07/2020",
+        value: priorDate,
       },
     ],
     workItemLink: "https://hackney.gov.uk",
@@ -50,7 +55,7 @@ const designData = [
       },
       {
         key: "dueCompletion",
-        value: "17/09/2019",
+        value: circleDate,
       },
     ],
     workItemLink: "https://google.co.uk",

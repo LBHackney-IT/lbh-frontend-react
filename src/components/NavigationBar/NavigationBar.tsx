e605d4e8cb54192b7d11ccf10a45284d75c38c06
Tony Griffin <tony@madetech.com>
2020-08-12 19:21:14 +0100

    Add link-text class to focus on removing border right of last list element

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -26,7 +26,7 @@ interface NavTarget {
 export const NavigationBar = (props: NavBarProps): React.ReactElement => {
   const renderLinks = (targets: NavTarget[]): React.ReactElement[] => {
     return targets.map((navTarget) => (
-      <li key={navTarget.name} data-test="navTarget-name">
+      <li key={navTarget.name} data-test="navTarget-name" className="link-text">
         <Link href={`${navTarget.url}`}>{navTarget.name}</Link>
       </li>
     ));

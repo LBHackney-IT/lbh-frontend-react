59384a41516181020b99c272a60b8d910c975360
Tony Griffin <tony@madetech.com>
2020-08-27 12:11:59 +0100

    Added lbh specific styling components to Tile component and Storybook setup
    Co-authored-by: Ana Bebic <ana.bebic@hackney.gov.uk>

--- src/components/Tile/Tile.tsx
+++ src/components/Tile/Tile.tsx
@@ -1,30 +1,33 @@
 import React, { Component, ReactChildren, ReactNode } from "react";
 import { DataAttributes } from "../../helpers/Attributes";
+import { Heading, HeadingLevels } from "../typography/Heading";
+import { Paragraph } from "../typography/Paragraph";
 
 export interface TileProps extends React.AriaAttributes, DataAttributes {
   /**
    * A href link address that is passed into the Tile component
    */
-  link?: string | undefined;
+  link?: string;
   /**
    * A title wrapped in an anchor tag.
    */
-  title?: string | undefined;
+  title?: string;
   /**
    * Value for the mailto input.
    */
-  children?: ReactNode | undefined;
-  //children?: React.ReactNodeArray
+  children?: ReactNode;
 }
 
 export const Tile = (props: TileProps): React.ReactElement => {
   return (
     <div className="lbh-container">
       <div className="lbh-container row details">
-        <a href={props.link} data-test="tile-link">
-          {props.title}
-        </a>
-        {props.children}
+        <Heading level={HeadingLevels.H3}>
+          <a href={props.link} data-test="tile-link">
+            {props.title}
+          </a>
+        </Heading>
+        <Paragraph>{props.children}</Paragraph>
       </div>
     </div>
   );
--- stories/Tile.stories.tsx
+++ stories/Tile.stories.tsx
@@ -1,28 +1,29 @@
 import { storiesOf } from "@storybook/react";
 import * as React from "react";
 import { Tile } from "../src/components/Tile/Tile";
+import { Paragraph } from "../src/components/typography/Paragraph";
 
 storiesOf("Tile", module).add("tile props", () => {
   const props = {
     link: "www.hackney.gov.uk",
     title: "Mrs Joan Fisher",
-    details: "03-04-2000",
   };
 
-  const arrTest = [
-    {
-      "Date of birth": "1970 - 02 - 30",
-      Mobile: "07777123456",
-      Home: "02088881234",
-      Work: "02012345678",
-      Email: "mjf@email.com",
-    },
-  ];
+  const test = (
+    <div>
+      <Paragraph>Date of birth: 01/08/2000</Paragraph>
+      <Paragraph>Mobile: 077070087654</Paragraph>
+      <Paragraph>Home: 020888812334</Paragraph>
+      <Paragraph>Work: 020777756789</Paragraph>
+      <Paragraph>
+        Email: <a href="mailto:someone@email.com">fake@dummy.com</a>
+      </Paragraph>
+    </div>
+  );
 
   return (
     <Tile link={props.link} title={props.title}>
-      <div>{props.details}</div>
-      <div>{arrTest}</div>
+      <div>{test}</div>
     </Tile>
   );
 });

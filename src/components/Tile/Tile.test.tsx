78311fc59793f33e98f8c08edab83ec0e0245971
LBHABEBIC <ana.bebic@hackney.gov.uk>
2020-09-04 09:12:29 +0100

    Update the changelog in preparation for v0.5.3

--- src/components/Tile/Tile.test.tsx
+++ src/components/Tile/Tile.test.tsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Tile } from "./Tile";
 import { Paragraph } from "../typography/Paragraph";
 import { create } from "react-test-renderer";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 describe("<Tile/>", () => {
-  it("renders the snapshot correctly", () => {
-    const TileOne = (
+  let TileOne: {} | null | undefined;
+  let TileTwo: {} | null | undefined;
+  beforeEach(() => {
+    TileOne = (
       <div>
         <Paragraph>Date of birth: 01/08/2000</Paragraph>
         <Paragraph>Mobile: 077070087654</Paragraph>
@@ -16,7 +18,7 @@ describe("<Tile/>", () => {
         </Paragraph>
       </div>
     );
-    const TileTwo = (
+    TileTwo = (
       <div>
         <Paragraph>Date of birth: 10/06/2010</Paragraph>
         <Paragraph>Mobile: 073070087234</Paragraph>
@@ -27,244 +29,119 @@ describe("<Tile/>", () => {
         </Paragraph>
       </div>
     );
+  });
+
+  it("renders the snapshot correctly", () => {
     const component = create(
-      <Tile
-        targets={[
-          {
-            link: "www.google.com",
-            title: "Mrs Sally Fisher",
-            children: TileOne,
-          },
-          {
-            link: "www.facebook.com",
-            title: "Ms Joan Fisher",
-            children: TileTwo,
-          },
-        ]}
-      />
+      <Tile link="www.google.com" title="Mrs Sally Fisher">
+        {TileOne}
+      </Tile>
     );
     expect(component).toMatchInlineSnapshot(`
       <div
-        className="govuk-container"
+        className="govuk-grid-row"
       >
-        <ul>
-          <nav>
-            <div
-              className="govuk-grid-row"
+        <div
+          className="govuk-grid-column-one-quarter"
+        >
+          <div
+            className="tile"
+            data-test="tile-container"
+          >
+            <h3
+              className="lbh-heading-h3"
             >
-              <div
-                className="govuk-grid-column-one-quarter"
+              <a
+                data-test="tile-link"
+                href="www.google.com"
               >
-                <div
-                  className="tile"
-                  data-test="tile-container"
-                >
-                  <h3
-                    className="lbh-heading-h3"
-                  >
-                    <a
-                      data-test="tile-link"
-                      href="www.google.com"
-                    >
-                      Mrs Sally Fisher
-                    </a>
-                  </h3>
-                  <p
-                    className="lbh-body"
-                  >
-                    <div>
-                      <p
-                        className="lbh-body"
-                      >
-                        Date of birth: 01/08/2000
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Mobile: 077070087654
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Home: 020888812334
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Work: 020777756789
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Email: 
-                        <a
-                          href="mailto:someone@email.com"
-                        >
-                          fake@dummy.com
-                        </a>
-                      </p>
-                    </div>
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div
-              className="govuk-grid-row"
+                Mrs Sally Fisher
+              </a>
+            </h3>
+            <p
+              className="lbh-body"
             >
-              <div
-                className="govuk-grid-column-one-quarter"
-              >
-                <div
-                  className="tile"
-                  data-test="tile-container"
+              <div>
+                <p
+                  className="lbh-body"
                 >
-                  <h3
-                    className="lbh-heading-h3"
+                  Date of birth: 01/08/2000
+                </p>
+                <p
+                  className="lbh-body"
+                >
+                  Mobile: 077070087654
+                </p>
+                <p
+                  className="lbh-body"
+                >
+                  Home: 020888812334
+                </p>
+                <p
+                  className="lbh-body"
+                >
+                  Work: 020777756789
+                </p>
+                <p
+                  className="lbh-body"
+                >
+                  Email: 
+                  <a
+                    href="mailto:someone@email.com"
                   >
-                    <a
-                      data-test="tile-link"
-                      href="www.facebook.com"
-                    >
-                      Ms Joan Fisher
-                    </a>
-                  </h3>
-                  <p
-                    className="lbh-body"
-                  >
-                    <div>
-                      <p
-                        className="lbh-body"
-                      >
-                        Date of birth: 10/06/2010
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Mobile: 073070087234
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Home: 020888812334
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Work: 020777756543
-                      </p>
-                      <p
-                        className="lbh-body"
-                      >
-                        Email: 
-                        <a
-                          href="mailto:someone@email.com"
-                        >
-                          fake@dummy.com
-                        </a>
-                      </p>
-                    </div>
-                  </p>
-                </div>
+                    fake@dummy.com
+                  </a>
+                </p>
               </div>
-            </div>
-          </nav>
-        </ul>
+            </p>
+          </div>
+        </div>
       </div>
     `);
   });
   it("renders the correct number of Tiles", () => {
-    const TileOne = (
-      <div>
-        <Paragraph>Date of birth: 01/08/2000</Paragraph>
-        <Paragraph>Mobile: 077070087654</Paragraph>
-        <Paragraph>Home: 020888812334</Paragraph>
-        <Paragraph>Work: 020777756789</Paragraph>
-        <Paragraph>
-          Email: <a href="mailto:someone@email.com">fake@dummy.com</a>
-        </Paragraph>
-      </div>
-    );
-    const TileTwo = (
-      <div>
-        <Paragraph>Date of birth: 10/06/2010</Paragraph>
-        <Paragraph>Mobile: 073070087234</Paragraph>
-        <Paragraph>Home: 020888812334</Paragraph>
-        <Paragraph>Work: 020777756543</Paragraph>
-        <Paragraph>
-          Email: <a href="mailto:someone@email.com">fake@dummy.com</a>
-        </Paragraph>
-      </div>
-    );
-    const component = shallow(
-      <Tile
-        targets={[
-          {
-            link: "www.google.com",
-            title: "Mrs Sally Fisher",
-            children: TileOne,
-          },
-          {
-            link: "www.facebook.com",
-            title: "Ms Joan Fisher",
-            children: TileTwo,
-          },
-        ]}
-      />
+    const component = mount(
+      <>
+        <Tile link="www.facebook.com" title="Ms Joan Fisher">
+          {TileTwo}
+        </Tile>
+        <Tile link="www.google.com" title="Mrs Sally Fisher">
+          {TileOne}
+        </Tile>
+      </>
     );
     expect(component.find({ "data-test": "tile-container" }).length).toBe(2);
   });
   it("Renders the correct title in the anchor tag", () => {
-    const TileOne = (
-      <div>
-        <Paragraph>Date of birth: 01/08/2000</Paragraph>
-        <Paragraph>Mobile: 077070087654</Paragraph>
-        <Paragraph>Home: 020888812334</Paragraph>
-        <Paragraph>Work: 020777756789</Paragraph>
-        <Paragraph>
-          Email: <a href="mailto:someone@email.com">fake@dummy.com</a>
-        </Paragraph>
-      </div>
-    );
     const component = shallow(
-      <Tile
-        targets={[
-          {
-            link: "www.google.com",
-            title: "Mrs Sally Fisher",
-            children: TileOne,
-          },
-        ]}
-      />
+      <Tile link="www.google.com" title="Mrs Sally Fisher">
+        {TileOne}
+      </Tile>
     );
     expect(component.find({ "data-test": "tile-link" }).text()).toBe(
       "Mrs Sally Fisher"
     );
   });
   it("Renders a full url in the href property in the anchor tag", () => {
-    const TileOne = (
-      <div>
-        <Paragraph>Date of birth: 01/08/2000</Paragraph>
-        <Paragraph>Mobile: 077070087654</Paragraph>
-        <Paragraph>Home: 020888812334</Paragraph>
-        <Paragraph>Work: 020777756789</Paragraph>
-        <Paragraph>
-          Email: <a href="mailto:someone@email.com">fake@dummy.com</a>
-        </Paragraph>
-      </div>
-    );
     const component = shallow(
-      <Tile
-        targets={[
-          {
-            link: "www.hackney.gov.uk",
-            title: "Mrs Sally Fisher",
-            children: TileOne,
-          },
-        ]}
-      />
+      <Tile link="www.hackney.gov.uk" title="Mrs Sally Fisher">
+        {TileTwo}
+      </Tile>
     );
+
     expect(component.find({ "data-test": "tile-link" }).prop("href")).toBe(
       "www.hackney.gov.uk"
     );
+  });
+  it("Renders contents of children props correctly", () => {
+    const childrenOutput =
+      "Mrs Sally FisherDate of birth: 10/06/2010Mobile: 073070087234Home: 020888812334Work: 020777756543Email: fake@dummy.com";
+    const component = mount(
+      <Tile link="www.hackney.gov.uk" title="Mrs Sally Fisher">
+        {TileTwo}
+      </Tile>
+    );
+
+    expect(component.text()).toEqual(childrenOutput);
   });
 });
